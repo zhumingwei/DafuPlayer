4f770a378a727ebf92bea623c3415fecb93c6b18
zhumingwei <zhumingwei2016@gmail.com>
2020-07-06 09:19:21 +0800

    temp

--- playerlibrary/src/main/cpp/MediaPlayer/source/player/AVMessageQueue.h
+++ playerlibrary/src/main/cpp/MediaPlayer/source/player/AVMessageQueue.h
@@ -6,7 +6,61 @@
 #define DAFUPLAYER_AVMESSAGEQUEUE_H
 
 
+extern "C" {
+#include <cstring>
+#include <libavutil/mem.h>
+};
+
+
+#include <Mutex.h>
+#include <Condition.h>
+#include "PlayerMessage.h"
+
+typedef struct AVMessage {
+    int what;
+    int arg1;
+    int arg2;
+    void *obj;
+
+    void (*free)(void *obj);
+
+    struct AVMessage *next;
+} AVMessage;
+
+inline static void message_init(AVMessage *msg) {
+    memset(msg, 0, sizeof(AVMessage));
+}
+
+inline static void message_free(void *obj) {
+    av_free(obj);
+}
+
 class AVMessageQueue {
+public:
+    AVMessageQueue();
+    virtual ~AVMessageQueue();
+
+    void start();
+    void stop();
+    void flush();
+    void release();
+    void postMessage(int what);
+    void postMessage(int what, int arg1);
+    void postMessage(int what, int arg1, int arg2);
+    void postMessage(int what, int arg1, int arg2, void *obj, int len);
+    int getMessage(AVMessage *msg);
+    int getMessage(AVMessage *msg,int block);
+    void removeMessage(int what);
+
+private:
+    int putMessage(AVMessage *msg);
+
+private:
+    Mutex mMutex;
+    Condition mCondition;
+    AVMessage *mFirstMsg, *mLastMsg;
+    bool abortRequest;
+    int mSize;
 
 };
 
---
+++ playerlibrary/src/main/cpp/MediaPlayer/source/player/PlayerMessage.h
@@ -0,0 +1,43 @@
+//
+// Created by zhumingwei on 2020/7/6.
+//
+
+#ifndef DAFUPLAYER_PLAYERMESSAGE_H
+#define DAFUPLAYER_PLAYERMESSAGE_H
+
+
+//Message what
+#define MSG_FLUSH               0x00    //默认
+#define MSG_ERROR               0x10    //出错回调
+#define MSG_PREPARED            0x20    //准备完成回调
+#define MSG_STARTED             0x30    //已经开始
+#define MSG_COMPLETED           0x40    //播放完成回调
+
+#define MSG_OPEN_INPUT          0x50    //打开文件
+#define MSG_FIND_STREAM_INFO    0x51    //查找媒体流信息
+#define MSG_PREPARE_DECODER     0X52    //准备解码器
+#define MSG_VIDEO_SIZE_CHANGE   0X53    //视频大小变化
+#define MSG_SAR_CHANGE          0X54    //长宽比变化
+#define MSG_AUDIO_START         0X55    //开始音频解码
+#define MSG_AUDIO_RENDERING_START 0X56  //音频渲染开始（播放开始）
+#define MSG_VIDEO_START         0X57    //开始视频解码
+#define MSG_VIDEO_RENDERING_START   0X58    //视频渲染开始
+#define MSG_VIDEO_ROTATION_CHANGE   0X59    //旋转角度变化
+
+#define MSG_BUFFERING_START         0X60    //缓冲开始
+#define MSG_BUFFERING_END           0X61    //缓冲完成
+#define MSG_BUFFERING_UPDATE        0X62    //缓冲更新
+#define MSG_BUFFERING_TIME_UPDATE   0X63    //缓冲时间更新
+
+#define MSG_SEEK_COMPLETE          0X70     //定位完成
+#define MSG_PLAYBACK_STATE_CHANGE  0X80     //播放状态变更
+#define MSG_TIMED_TXT               0X90    //字幕
+
+#define MSG_REQUEST_PREPARE         0X200   //异步请求准备
+#define MSG_REQUEST_START           0X201   //异步请求开始
+#define MSG_REQUEST_PAUSE           0X202   //请求暂停
+#define MSG_REQUEST_SEEK            0X203   //请求定位
+
+#define MSG_CURRENT_POSITION        0X300   //当前时钟
+
+#endif //DAFUPLAYER_PLAYERMESSAGE_H
